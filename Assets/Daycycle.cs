3d822389e9154d5d06f976ab02ef2ef64e3dabbb
John Turner <groups@norgg.org>
2014-08-25 00:42:12 +0100

    Red sunrise

--- Assets/Daycycle.cs
+++ Assets/Daycycle.cs
@@ -9,14 +9,15 @@ public class Daycycle : MonoBehaviour {
 
 	void Start () {
 		lightCol = light.color;
-		setCol = new Color(1, lightCol.g, lightCol.b);
+		setCol = new Color(32f, lightCol.g, lightCol.b);
 	}
 	
 	void Update () {
 		transform.Rotate(new Vector3(0.1f, 0, 0));
 		sunlight = 0.6f + Mathf.Sin((transform.localEulerAngles.x) * Mathf.Deg2Rad)/2;
-		if (sunlight > 0.55 && sunlight < 0.65) {
-			light.color = Color.Lerp(setCol, lightCol, (sunlight-0.55f)*10);
+		if (sunlight > 0.4 && sunlight < 0.8) {
+			Debug.Log("Lerping light: " + (sunlight-0.6f)*2.5);
+			light.color = Color.Lerp(setCol, lightCol, (sunlight-0.6f)*5);
 		} else {
 			light.color = lightCol;
 		}
